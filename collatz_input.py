07c899cd78efe1c16d49896b9287fe119bd933a5
SimonMcLain <35738270+SimonMcLain@users.noreply.github.com>
2018-02-12 22:55:13 +0000

    Added user integer input ot Collatz file
    Update was added after exercise was submitted. Therefore not requesting to be graded against this file. Uploading for my future references

---
+++ collatz_input.py
@@ -0,0 +1,12 @@
+# Simon McLain, 2018-2-12
+#Collatz Conjecture added user request to input integer
+
+n = int(input("Please enter an integer: "))
+
+while n > 1:
+    if n % 2 == 0:
+        n = n / 2
+        print(n) 
+    else:
+        n = (n * 3) + 1
+        print(n)
