215c278da9c6f2f0c5869ca10afa48c4c3e793b3
Simon McLain <G00364775@GMIT.iegit config --global user.name SimonMcLaingit config --global user.email G00364775@GMIT.ie>
2018-03-27 16:19:30 +0100

    Notes added to exercise 5 and vscode files added to folder

--- Exercise_5_v1.0.py
+++ Exercise_5_v1.0.py
@@ -10,15 +10,17 @@
 import csv
 """imports a module to enable reading of a comma separated values file"""
 
-with open("data/iris.csv") as f:
+with open("iris.csv") as f:
   """opens the file and asigns content as the variable f""" 
   for line in f:
     """this for statement instructs Python to interate over each line in the file, which is asigned as f"""
     a = line.split(',')
     """this statement splits the line by the commas and assigns each value to the variable a""" 
+    
+    
     print (a[0], a[1], a[2], a[3])
     """this instructs Python to print the first four values of each line"""
-    
+
     
 
     
