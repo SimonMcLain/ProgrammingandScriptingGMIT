ac73eaa8a0429a676c3f4a6b0c563dde34c11b51
Simon McLain <G00364775@GMIT.iegit config --global user.name SimonMcLaingit config --global user.email G00364775@GMIT.ie>
2018-03-27 18:18:37 +0100

    Minor edits made to notes

--- Exercise_4_v1.1.py
+++ Exercise_4_v1.1.py
@@ -2,7 +2,7 @@
 # Second attempt as v.1.0 ran very slowly on my computer. Solution base on code found at: 
 # https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 # I modified the code slightly so it would run, changing xrange to range and adding parentheses to the print statements.
-# This code runs much faster than my original 
+# This code runs much faster than my original v1.0
 
 check_list = [11, 13, 14, 16, 17, 18, 19, 20]
 
