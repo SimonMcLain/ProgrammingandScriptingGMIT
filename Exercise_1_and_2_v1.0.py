5157cf26a7eed67b890e628f70081fab7960c161
Simon McLain <G00364775@GMIT.iegit config --global user.name SimonMcLaingit config --global user.email G00364775@GMIT.ie>
2018-03-29 18:05:11 +0100

    Comments added to script

--- Exercise_1_and_2_v1.0.py
+++ Exercise_1_and_2_v1.0.py
@@ -1,28 +1,47 @@
-# Simon McLain, edited from a script created by Ian McLoughlin on 2018-02-12 
+# Simon McLain, edited from a script created by Ian McLoughlin on 2018-02-12: 
 # https://github.com/ianmcloughlin/python-fib/blob/master/fibname.py
-# Week 2 Exercise for Programming and Scripting
-# This program coverts the first and last letter of someones name into a fibonaci number.
-# The ord() functionThe ord() function is used to return an integer of the given single Unicode character. 
-# The returned integer represents the Unicode code point.(reference: https://www.jquery-az.com)
+
+
+# Week 1 exercise 1:
+"""Calculate the nth Fibonacci number where n the sum of the first and last letters of your first name as numbers. and
+post on the discussion forum: My name is Simon, so the first and last letter of my name (S + N = 19 + 14) give the number  33,
+the 33rd Fibonacci number is 352,4578."""
+
+# Week 2 exercise 2: 
+"""This program coverts the first and last Unicode characteres of someones surname into integers, adds them together
+and returns the corresponding fibonacci number for their sum. It also prints the definition of the ord() function."""
 
 def fib(n):
-  """This function returns the nth Fibonacci number."""
+  """defines n as
+  a fibonacci number, fib() 
+  is is a function in the Python standard library)"""
   i = 0
   j = 1
   n = n - 1
-  """Assigns values to variables i, j & n for the calculation"""
+  """assigns initial values to the varibles
+  i & j, assigns a calculation to the variable n"""
   while n >= 0:
     i, j = j, i + j
     n = n - 1
   return i
-name = "Simon"
+  """interates over the loop provided (while!') n is
+  greater than or equal to 0, preventing negative counting"""
+name = "McLain"
 first = name[0]
 last = name[-1]
+"""creates the container 'name' for my surname
+and then assigns the 'first' and 'last' letters"""
 firstno = ord(first)
 lastno = ord(last)
 x = firstno + lastno
 ans = fib(x)
-print("My first name is", name)
-print("The first letter", first, "is number", firstno)
-print("The last letter", last, "is number", lastno)
-print("Fibonacci number", x, "is", ans)
+"""Converts first and last Unicode characters of my surname to integers, adds them together
+and returns the corresponding fibonacci number. Ord() is a function in the Python standard library"""
+print("My surname is:", name)
+print("The first letter", first, "is number:", firstno)
+print("The last letter", last, "is number:", lastno)
+print("Fibonacci number", x, "is:", ans)
+
+print("The ord() function is used to return an integer of the given single Unicode character. The returned integer represents the Unicode code point.(reference: https://www.jquery-az.com)")
+
+"""Print statements to return the output of the script to the terminal""" 
