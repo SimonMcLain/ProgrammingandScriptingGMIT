2e867654e086f004a16d2dbe5ad0fda4b2462672
Simon McLain <G00364775@GMIT.iegit config --global user.name SimonMcLaingit config --global user.email G00364775@GMIT.ie>
2018-03-29 18:24:18 +0100

    Added comments to the script

--- Exercise_3_v1.0.py
+++ Exercise_3_v1.0.py
@@ -1,12 +1,21 @@
 # Simon McLain, 2018-2-11
-#Collatz Conjecture
+
+"""Write a single Python script that starts with an integer and repeatedly applies the Collatz function 
+(divide by 2 if even, multiply by three and 1 if odd) using a while loop and if statement. At each iteration, 
+the current value of the integer should be printed to the screen. 
+You can specify in your code the starting value of 17.""" 
 
 n = 17
-
+"""assigns the variable n to the range 0 to 17"""
 while n > 1:
-    if n % 2 == 0:
-        n = n / 2
-        print(n) 
-    else:
-        n = (n * 3) + 1
-        print(n)
+  """starts the loop interating at the integer 2"""
+  if n % 2 == 0:
+    n = n / 2
+    print(n) 
+    """The if statement finds even integers 
+    by checking the reminder equals 0 when divided by 2"""
+  else:
+    n = (n * 3) + 1 
+    print(n)
+    """The else statement deals with odd integers that are 'skipped' by the if statement
+    multiplies them by 3 and prints the result"""
--- Exercise_3_v1.1.py
+++ Exercise_3_v1.1.py
@@ -2,7 +2,8 @@
 # Collatz Conjecture added user request to input integer
 
 n = int(input("Please enter an integer: "))
-
+"""asks the user to provide the integer and 
+assigns it to the variable n"""
 while n > 1:
     if n % 2 == 0:
         n = n / 2
